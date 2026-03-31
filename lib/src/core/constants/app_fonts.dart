27563f0fcc7032d72f102b071f1f8899eb8c95e1
idris-ghamid <idris.ghamid@gmail.com>
2026-03-31 02:41:29 +0200

    feat(fonts): add Arabic fonts collection for Azkar share feature
    - Add 17 Arabic fonts (Amiri, Cairo, Aref Ruqaa, etc.)
    - Create AppFonts class for centralized font management
    - Fonts used in Azkar image sharing and Quran text styling

---
+++ lib/src/core/constants/app_fonts.dart
@@ -0,0 +1,23 @@
+class AppFonts {
+  static const List<String> customFonts = [
+    'IDRISIUM',
+    'Amiri-Regular',
+    'AmiriQuran-Regular',
+    'Aref Ruqaa Bold',
+    'ASane-Jaleh',
+    'b-helal',
+    'bader-lamsat',
+    'BritishCouncil-Arabic-Black',
+    'Cairo-Black',
+    'Cairo-Bold',
+    'Cairo-Light',
+    'Cairo-Regular',
+    'Cairo-SemiBold',
+    'a-massir-ballpoint',
+    'Abd-ElRady-Regular',
+    'Afsaneh-Font',
+    'al-hadaribold',
+    'al-majd',
+    'KFGQPC-Uthmanic-HAFS-Regular',
+  ];
+}
