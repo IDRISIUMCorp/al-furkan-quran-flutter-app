c9ca321fd4dd9a9077d82ff626bcda2d12c2f249
idris-ghamid <idris.ghamid@gmail.com>
2026-04-03 14:18:59 +0200

    fix(startup): open mushaf مباشرة and bypass onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import "dart:async";
 import "dart:developer";
 import "dart:ui";
 
@@ -21,13 +22,13 @@ import "package:al_quran_v3/src/resources/translation/languages.dart";
 import "package:al_quran_v3/src/screen/location_handler/cubit/location_data_qibla_data_cubit.dart";
 import "package:al_quran_v3/src/screen/location_handler/model/location_data_qibla_data_state.dart";
 import "package:al_quran_v3/src/screen/prayer_time/cubit/prayer_time_cubit.dart";
-import "package:al_quran_v3/src/screen/quran_bootstrap/quran_bootstrap_page.dart";
 import "package:al_quran_v3/src/screen/quran_reader/cubit/reader_ui_cubit.dart";
 import "package:al_quran_v3/src/screen/quran_script_view/cubit/ayah_by_ayah_in_scroll_info_cubit.dart";
 import "package:al_quran_v3/src/screen/quran_script_view/cubit/ayah_to_highlight.dart";
 import "package:al_quran_v3/src/screen/quran_script_view/cubit/landscape_scroll_effect.dart";
 import "package:al_quran_v3/src/screen/settings/cubit/quran_script_view_cubit.dart";
 import "package:al_quran_v3/src/screen/setup/cubit/resources_progress_cubit_cubit.dart";
+import "package:al_quran_v3/src/screen/mushaf/mushaf_screen.dart";
 import "package:al_quran_v3/src/theme/app_theme.dart";
 import "package:al_quran_v3/src/theme/controller/theme_cubit.dart";
 import "package:al_quran_v3/src/theme/controller/theme_state.dart";
@@ -89,9 +90,11 @@ Future<void> main() async {
   final prefs = await SharedPreferences.getInstance();
   await configureDependencies(preferences: prefs);
 
-  final bootstrapSnapshot = await getIt<AppBootstrapCoordinator>().prepareApp(
+  final bootstrapCoordinator = getIt<AppBootstrapCoordinator>();
+  final bootstrapSnapshot = await bootstrapCoordinator.prepareApp(
     loadLocationState: LocationQiblaPrayerDataCubit.getSavedState,
   );
+  await bootstrapCoordinator.prepareLaunch();
 
   log(bootstrapSnapshot.locationState.madhab.toString(), name: "Madhab");
 
@@ -100,6 +103,14 @@ Future<void> main() async {
       initialLocale: bootstrapSnapshot.initialLocale,
       locationQiblaPrayerDataState: bootstrapSnapshot.locationState,
     ),
+  );
+  unawaited(
+    bootstrapCoordinator.runDeferredWarmup().catchError((error, stackTrace) {
+      log(
+        "Deferred warmup failed after app launch: $error\n$stackTrace",
+        name: "AppBootstrapWarmup",
+      );
+    }),
   );
   platform_services.hideLoadingIndicator();
 }
@@ -210,7 +221,7 @@ class MyApp extends StatelessWidget {
                         );
                       },
                       scrollBehavior: AppScrollBehavior(),
-                      home: const QuranBootstrapPage(),
+                      home: const MushafScreen(),
                     );
                   },
                 );
